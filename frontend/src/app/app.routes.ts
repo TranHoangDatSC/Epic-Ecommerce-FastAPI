a146a0e063739ffc5db96143c726a88eed099c5d
nhanthe500113 <nhanthe500113@gmail.com>
2026-03-21 22:15:28 +0700

    Thêm đường dẫn \contact

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,38 +4,43 @@ export const routes: Routes = [
   {
     path: '',
     redirectTo: '/home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'home',
-    loadComponent: () => import('./features/home/home').then(m => m.HomeComponent)
+    loadComponent: () => import('./features/home/home').then((m) => m.HomeComponent),
   },
   {
     path: 'shop',
-    loadComponent: () => import('./features/shop/shop').then(m => m.ShopComponent)
+    loadComponent: () => import('./features/shop/shop').then((m) => m.ShopComponent),
   },
   {
     path: 'auth',
-    loadChildren: () => import('./features/auth/auth.routes').then(m => m.authRoutes)
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.authRoutes),
   },
   {
     path: 'customer',
-    loadChildren: () => import('./features/customer/customer.routes').then(m => m.customerRoutes)
+    loadChildren: () => import('./features/customer/customer.routes').then((m) => m.customerRoutes),
   },
   {
     path: 'seller',
-    loadChildren: () => import('./features/seller/seller.routes').then(m => m.sellerRoutes)
+    loadChildren: () => import('./features/seller/seller.routes').then((m) => m.sellerRoutes),
   },
   {
     path: 'admin',
-    loadChildren: () => import('./features/admin/admin.routes').then(m => m.adminRoutes)
+    loadChildren: () => import('./features/admin/admin.routes').then((m) => m.adminRoutes),
   },
   {
     path: 'moderator',
-    loadChildren: () => import('./features/moderator/moderator.routes').then(m => m.moderatorRoutes)
+    loadChildren: () =>
+      import('./features/moderator/moderator.routes').then((m) => m.moderatorRoutes),
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./features/contact/contact').then((m) => m.ContactComponent),
   },
   {
     path: '**',
-    redirectTo: '/home'
-  }
+    redirectTo: '/home',
+  },
 ];
