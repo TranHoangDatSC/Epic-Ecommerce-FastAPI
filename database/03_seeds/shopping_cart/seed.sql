c114509e417d1926edfbc9bb3d383db8c0dcfd68
TranHoangDatSC <hoangdattran12@gmail.com>
2026-04-15 15:06:03 +0700

    seeding more data

--- database/03_seeds/shopping_cart/seed.sql
+++ database/03_seeds/shopping_cart/seed.sql
@@ -5,3 +5,13 @@
 INSERT INTO shopping_cart (user_id, last_updated) VALUES
 (3, CURRENT_TIMESTAMP),  -- user1
 (4, CURRENT_TIMESTAMP);  -- user2
+(8, CURRENT_TIMESTAMP),  -- user3
+(8, CURRENT_TIMESTAMP);  -- user4
+(9, CURRENT_TIMESTAMP),  -- user5
+(10, CURRENT_TIMESTAMP);  -- user6
+(11, CURRENT_TIMESTAMP),  -- user7
+(12, CURRENT_TIMESTAMP);  -- user8
+(13, CURRENT_TIMESTAMP),  -- user9
+(14, CURRENT_TIMESTAMP);  -- user10
+(15, CURRENT_TIMESTAMP);  -- user11
+
